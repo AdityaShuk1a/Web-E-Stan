b60f9dc8aec468b92a31588852d2efaa50b5a64c
FarSpawn <abhijitabhi127@gmail.com>
2025-01-30 19:01:02 +0530

    basic form of nav bar, completely responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import React, { useEffect } from 'react'
 import Preloader from './components/Preloader'
 import { PreloaderAnimation } from './animations/Animations'
+import LandingPage from './pages/LandingPage.jsx'
+
 const App = () => {
   useEffect(() => {
     PreloaderAnimation()
   },[])
+  
   return (
-
-    
-    <>
+    <div className="app-container">
       <Preloader />
-
-    </>
-    
+      <LandingPage />
+    </div>
   )
 }
 
