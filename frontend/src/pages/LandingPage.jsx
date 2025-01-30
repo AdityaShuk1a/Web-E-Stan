2df47d6c96df38d78c51526297fee9d5152c6c22
FarSpawn <abhijitabhi127@gmail.com>
2025-01-30 22:36:34 +0530

    goin for the subject page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -5,7 +5,24 @@ const LandingPage = () => {
   return (
     <div className="LandingPageBase bg-gray-50">
       <Navbar />
-      
+      <div className="TagContainer">
+        <span>M</span>
+        <span>O</span>
+        <span>O</span>
+        <span>N</span>
+        <span>B</span>
+        <span>E</span>
+        <span>A</span>
+        <span>M</span>
+        <br />
+        <span>A</span>
+        <span>C</span>
+        <span>A</span>
+        <span>D</span>
+        <span>E</span>
+        <span>M</span>
+        <span>Y</span>
+      </div>
     </div>
   )
 }
